14e9b80c38aee37b2a6b698611da88f169dd8706
RuchikaBais <ruchikasbais@gmail.com>
2024-05-23 20:56:01 +0530

    uploading UI code

---
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -0,0 +1,44 @@
+import "./profileUpdatePage.scss";
+
+function ProfileUpdatePage() {
+  return (
+    <div className="profileUpdatePage">
+      <div className="formContainer">
+        <form>
+          <h1>Create Profile</h1>
+          <div className="item">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Enter your name.."
+            />
+          </div>
+          <div className="item">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email.."
+            />
+          </div>
+          <div className="item">
+            <label htmlFor="password">Password</label>
+            <input id="password" name="password" type="password"
+            placeholder="Password" />
+          </div>
+          <button>Update</button>
+          <a href="/profile"> {'<-'} Back</a>
+        </form>
+      </div>
+      
+      <div className="sideContainer">
+        <img src="/bg.png" alt="" className="avatar" />
+      </div>
+    </div>
+  );
+}
+
+export default ProfileUpdatePage;
